060ec79d887503fad2eccd566d0a48df1b4a1525
tanzidakazi <ktanzida@gmail.com>
2018-10-23 13:37:52 +0600

    dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,4 +1,4 @@
-'''# keys() values() items() Method
+# keys() values() items() Method
 spam ={'color': 'black',
        'age': '24'}
 for v in spam.values():
@@ -23,6 +23,30 @@ picnicItems ={'apples': 5,
               'cups': 2}
 print('I am bringing ' + str(picnicItems.get('cups', 0)) + ' cups')
 print('I am bringing ' + str(picnicItems.get('eggs', 0)) + ' eggs')
-'''
-# setdefault() Method
 
+# setdefault() Method
+spam = {'name': 'Tanzida',
+        'age': 24}
+spam.setdefault('color', 'black')
+print(spam)
+print("No change in color")
+spam.setdefault('color', 'white')
+print(spam)
+
+#characterCount. A short program that counts the number of occurrences of each letter in a string. Pretty Printing
+import pprint
+message= 'It was a bright cold day in April, and the clocks were striking thirteen.'
+countCharacter ={}
+
+for character in message:
+    countCharacter.setdefault(character, 0)
+    countCharacter[character]= countCharacter[character]+ 1
+print(countCharacter)
+pprint.pprint(countCharacter)
+#pprint(pprint.pformat(countCharacter))
+
+
+
+
+
+    
