57aef7ee718be9e0243136ee2bf4bb94a5bece77
tanzidakazi <ktanzida@gmail.com>
2018-10-25 14:02:52 +0600

    Chapter 4 & 6

---
+++ ListAndString.py
@@ -0,0 +1,18 @@
+# index() List Method
+# insert() List Method
+# append() List Method
+# remove() List Method
+# sort() List Method
+# copy() Function
+# deepcopy() Funtion
+
+============================================================================
+
+# upper(), lower() String Method
+# isupper(), islower() String Method
+# isX String Method
+# startswith(), endswith() String Method
+# join(), split() String Method
+# rjust(), ljust(), center() Justifying Text
+# strip(), rstrip(), lstrip() Removing Whitespace
+# Copying and Pasting with paperclip module
