ced82220311fea7b1c5e928b5e41be0d6340c42f
tanzidakazi <ktanzida@gmail.com>
2018-10-26 23:23:10 +0600

    Chapter 4, 6

--- ListAndString.py
+++ ListAndString.py
@@ -1,12 +1,42 @@
 # index() List Method
-# insert() List Method
 # append() List Method
+# insert() List Method
 # remove() List Method
 # sort() List Method
+
+friendList=['Mahmuda', 'Mazdia', 'Samiha', 'Sherin', 'Shegufta', 'abc', 'qwe']
+
+print('Index of Mahmuda is ')
+print(friendList.index('Mahmuda'))
+
+for i in range(len(friendList)):
+    print('Index: ' + str(i+1) + ', Name: ' + friendList[i])
+
+for i in range(len(friendList)):
+    print(friendList.index('Mazdia'))
+    print(friendList.index(friendList[i])) # same as str(i)
+
+print('Enter name of new friend: ')
+newFriend = input()
+friendList.append(newFriend)
+print(friendList)
+
+friendList.insert(2, 'Areeba')
+print(friendList)
+
+friendList.remove('abc')
+print(friendList)
+
+friendList.sort()
+print(friendList)
+friendList.sort(reverse=True)
+print(friendList)
+friendList.sort(key=str.lower)
+print(friendList)
+
+
 # copy() Function
 # deepcopy() Funtion
-
-============================================================================
 
 # upper(), lower() String Method
 # isupper(), islower() String Method
